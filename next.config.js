739ab7b8d4fe3f4136ef17f3e9602c88983ecc7b
lilian <lilian4218@gmail.com>
2023-07-18 11:29:44 +0800

    next 靜態網頁github

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
+
   reactStrictMode: true,
+
+  images: {
+
+    loader: 'akamai',
+
+    path: '',
+
+  },
+
+  basePath: '/next-temple',
+
+  assetPrefix: '/next-temple',
+
 }
 
 module.exports = nextConfig
